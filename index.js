fff4d00f44434408f13a6e09c5e13cdb229aafcc
Narendra Vishwakarma <narendra@studiographene.com>
2023-08-22 14:30:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const express = require('express')
+var cors = require('cors-express');
+
 const app = express()
+
+
+const options = {
+allow : {
+    origin: 'https://lazy-erin-turkey-sari.cyclic.cloud/',
+    methods: 'GET,PATCH,PUT,POST,DELETE,HEAD,OPTIONS',
+    headers: 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override'
+}
+//     ,
+// expose :{
+//     headers : null
+// },
+// max : {
+//     age : null
+// }
+//     ,
+// options : function(req, res, next){
+//     if (req.method == 'OPTIONS') {
+//         res.status(200).end();
+//     } else {
+//         next();
+//     }
+// }
+}
+
+app.use(cors(options));
+
+
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send({status: 'Yo!'})
