c050efe2e47d0866be8a3cefb50c32b5696853c4
NarendraSG <narendra@studiographene.com>
2023-10-25 11:44:53 +0000

    small fix

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const app = express()
 
 app.use(parser.json());
 
-// const options = {
-//     allow: {
-//         origin: ["*"],//'https://lazy-erin-turkey-sari.cyclic.cloud/',
-//         methods: 'GET,PATCH,PUT,POST,DELETE,HEAD,OPTIONS',
-//         headers: 'Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override'
-//     }
-// }
+const options = {
+    allow: {
+        origin: ["http://localhost:3000", "https://lazy-erin-turkey-sari.cyclic.cloud/"],//'https://lazy-erin-turkey-sari.cyclic.cloud/',
+        methods: 'GET,PATCH,PUT,POST,DELETE,HEAD,OPTIONS',
+        headers: '*'
+    }
+}
 
-app.use(cors());
+app.use(cors(options));
 
 
 
